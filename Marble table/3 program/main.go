a639721306aa4610e618f3258e7b9396d509a7bc
jaenmo <j.enjuto.monje@gmail.com>
2022-07-18 14:18:35 +0100

    update

--- Marble table/3 program/main.go
+++ Marble table/3 program/main.go
@@ -23,20 +23,73 @@ type sphere struct{
 	radius float64
 	density float64
 }
+
 func (s sphere) weight() float64{
 	return 3/4 * math.Pi * s.radius * s.radius * s.radius
 }
 
-type composed struct{
-	shape []shape
+type shape interface{
+	weight()float64
 }
+
+type composed struct{
+	shapes []shape
+}
+
 func (co composed) weight() float64{
-	shape := 
-	
-	return 
+	shapes := []shape {cube{}, sphere{}}
+	for _, v := range shapes{
+		co.weight() = shape.weight()
+	}
+	return co.weight()
 }
 
 func main(){
+	
+	
+	fmt.Println("What is the shape of the table?")
+	sliceShapes := []string {cube, sphere, composed}
+	for i, v := range sliceShapes {
+		fmt.Println(i+1, v)
+	}
+	var table int
+	fmt.Scanln(&table)
+	
+	if table == 1 {
+	parametersCube()
+
+	chooseMaterial()
+	var mat int
+	fmt.Scanln(&mat)
+	fmt.Println(cube.weight)
+
+	}else if table == 2{
+		
+		parametersSphere()
+		chooseMaterial()
+		var mat int
+		fmt.Scanln(&mat)
+	}else if table == 3{
+		
+		parametersCube()
+		parametersSphere()
+
+		chooseMaterial()
+		var mat int
+		fmt.Scanln(&mat)
+	}
+
+
+}
+func chooseMaterial(){
+	fmt.Println("Choose the material")
+	material := []string{"wood", "marble", "plastic"}
+	for i, v := range material{
+		fmt.Println(i+1, v)
+	}		
+}
+
+func parametersCube() {
 	fmt.Println("Insert length in cm")
 	var l float64
 	fmt.Scanln(&l)
@@ -48,24 +101,10 @@ func main(){
 	fmt.Println("Insert thikness in cm")
 	var t float64
 	fmt.Scanln(&t)
-
-	fmt.Println("Choose the material")
-	material := []string{"wood", "marble", "plastic"}
-	for i, v := range material{
-		fmt.Println(i+1, v)
-	}
-	fmt.Scanln(&m)
-	var 
-	//total := table{length: l, width: w, thickness: t, density: density {2.52, 1.5, 0.9} }
-
-	table.volume(l, w, t)
-	
-	
-	
-
 }
 
-func (t table) volume(float64, float64, float64){
-	v := t.length * t.width * t.thickness
-	fmt.Sprintln(v)
+func parametersSphere() {
+	fmt.Println("Insert radius in cm")
+	var r float64
+	fmt.Scanln(&r)
 }
